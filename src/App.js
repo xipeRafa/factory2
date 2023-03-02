56e294db369e02c3e4e0b78c33560044e2ee1b67
fer <fer@fers-iMac.local>
2023-03-01 20:16:17 -0700

    bien

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { WebsiteList } from "./components/WebsiteList";
 import { WebsiteForm } from "./components/WebsiteForm";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./components/Layout";
+
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
           <Route path="/list" element={<WebsiteList />} /> 
           <Route path="add" element={<WebsiteForm />} />
           <Route path="edit/:id" element={<WebsiteForm />} />
-          <Route path="*"  element={<WebsiteList />}/>
+          <Route path="*"  element={<Navigate to="/add" />}/>
         </Route>
       </Routes>
     </BrowserRouter>
