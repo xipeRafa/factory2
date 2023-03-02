4ade1541bd1c6eb4b4e7bf13fdb8fbbc5194c5f9
fer <fer@fers-iMac.local>
2023-03-01 18:09:03 -0700

    d

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
           <Route path="/" element={<WebsiteList />} />
           <Route path="add" element={<WebsiteForm />} />
           <Route path="edit/:id" element={<WebsiteForm />} />
-          <Route path="*" element={<h1>Not Found</h1>} />
+          <Route path="*"  element={<WebsiteList />}/>
         </Route>
       </Routes>
     </BrowserRouter>
